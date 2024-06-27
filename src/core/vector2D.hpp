08b7d24b0aa09b4c9a1471a0e3d9b7243ed5b917
Rechie Kho <50512341+RechieKho@users.noreply.github.com>
2024-06-27 17:50:43 +0800

    Alias numeric type.

--- src/core/vector2D.hpp
+++ src/core/vector2D.hpp
@@ -7,20 +7,22 @@
 template <typename Type>
 concept IsVector2D = requires(Type p_vector) {
     IsArithmatical<Type>;
+    IsSintTypeAvailable<Type>;
+    IsFloatTypeAvailable<Type>;
 
-    { p_vector.x } -> IsSameType<Float &>;
-    { p_vector.y } -> IsSameType<Float &>;
-    { p_vector.w } -> IsSameType<Float &>;
-    { p_vector.h } -> IsSameType<Float &>;
+    { p_vector.x } -> IsSameType<typename Type::FloatType &>;
+    { p_vector.y } -> IsSameType<typename Type::FloatType &>;
+    { p_vector.w } -> IsSameType<typename Type::FloatType &>;
+    { p_vector.h } -> IsSameType<typename Type::FloatType &>;
 
-    { p_vector *declval<Sint>() } -> IsSameType<Type>;
-    { p_vector *declval<Float>() } -> IsSameType<Type>;
-    { p_vector / declval<Sint>() } -> IsSameType<Type>;
-    { p_vector / declval<Float>() } -> IsSameType<Type>;
+    { p_vector *declval<typename Type::SintType>() } -> IsSameType<Type>;
+    { p_vector *declval<typename Type::FloatType>() } -> IsSameType<Type>;
+    { p_vector / declval<typename Type::SintType>() } -> IsSameType<Type>;
+    { p_vector / declval<typename Type::FloatType>() } -> IsSameType<Type>;
 
-    { p_vector.dot(p_vector) } -> IsSameType<Float>;
-    { p_vector.compute_length() } -> IsSameType<Float>;
-    { p_vector.compute_length_squared() } -> IsSameType<Float>;
+    { p_vector.dot(p_vector) } -> IsSameType<typename Type::FloatType>;
+    { p_vector.compute_length() } -> IsSameType<typename Type::FloatType>;
+    { p_vector.compute_length_squared() } -> IsSameType<typename Type::FloatType>;
 };
 
 #endif // VECTOR2D_HPP
